bf2912ca3ec164f21ae65f2164c448b4434fc02a
divyendh <159880316+divyendh@users.noreply.github.com>
2026-06-20 17:05:48 +0530

    project structure

---
+++ src/test/java/com/mykare/appointment_service/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mykare.appointment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
